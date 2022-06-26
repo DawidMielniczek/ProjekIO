fb70b2c4335b5528610190162004296aae1fe5de
Dawid Mielniczek <w61922@student.wsiz.edu.pl>
2022-06-26 21:56:05 +0200

    Funkcjonalność logowania

--- WypożyczalniaSprzętuFilmowego/WypożyczalniaSprzętuFilmowego/Strony/Logowanie/LogowanieAdmin.aspx.cs
+++ WypożyczalniaSprzętuFilmowego/WypożyczalniaSprzętuFilmowego/Strony/Logowanie/LogowanieAdmin.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WypożyczalniaSprzętuFilmowego.Api;
 
 namespace WypożyczalniaSprzętuFilmowego.Strony.Logowanie
 {
@@ -12,6 +13,27 @@ namespace WypożyczalniaSprzętuFilmowego.Strony.Logowanie
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void Btn_Click(object sender, EventArgs e)
+        {
+            var result = true;
+            WypożyczalniaApi api = new WypożyczalniaApi();
+            var services = api.GetAdmin();
+            for (int i = 0; i < services.Count; i++)
+            {
+                if (services[i].Email.Trim() == TxtloginA.Text && services[i].Haslo.Trim() == TxthasloA.Text)
+                {
+                    Session["Id"] = services[i].IdAdmina;
+                    Response.Redirect("~/Strony/Administrator/AdminGlowna.aspx");
+                    result = false;
+                    break;
+                }
+            }
+            if (result)
+            {
+                txtwynik.Text = "Podano błędne dane";
+            }
         }
     }
 }
