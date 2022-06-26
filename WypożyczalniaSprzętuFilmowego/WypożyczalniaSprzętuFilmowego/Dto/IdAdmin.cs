3eb63a53ac35a39016f0c37ccb271952771f23fd
Dawid Mielniczek <w61922@student.wsiz.edu.pl>
2022-06-26 21:21:36 +0200

    Dodanie DTO

---
+++ WypożyczalniaSprzętuFilmowego/WypożyczalniaSprzętuFilmowego/Dto/IdAdmin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WypożyczalniaSprzętuFilmowego.Dto
+{
+    public class IdAdmin
+    {
+        public int IdAdmina { get; set; }
+        public string Email { get; set; }
+        public string Haslo { get; set; }
+    }
+}
---
+++ WypożyczalniaSprzętuFilmowego/WypożyczalniaSprzętuFilmowego/Dto/IdOpini.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WypożyczalniaSprzętuFilmowego.Dto
+{
+    public class IdOpini
+    {
+        public int IdOpinii { get; set; }
+        public string Opis { get; set; }
+        public int Ocena { get; set; }
+    }
+}
